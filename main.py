52c4f7f9028314c781c09fc5b5ff178aa9fda1db
Andrew Arendt <andrewarendt@gmail.com>
2022-01-15 18:11:15 -0600

    404 handler

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
 from starlette.staticfiles import StaticFiles
+from starlette.exceptions import ExceptionMiddleware
+from starlette.responses import Response
 
 asgi_func = StaticFiles(directory='client/dist', html=True)
 
-async def app(scope, receive, send):
+async def ap(scope, receive, send):
     assert scope['type'] == 'http'
     await asgi_func(scope, receive, send)
+
+
+async def NOT_FOUND(request, exception):
+    return Response(status_code= 404)
+
+app = ExceptionMiddleware(ap, handlers = {404: NOT_FOUND})
