33bfd0142a7bfded77f12f81e3cdc2391924d1ab
Andrew Arendt <AndrewArendt@gmail.com>
2023-02-28 17:46:50 -0600

    progress

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ dev: clean
 	pip install -r requirements.txt
 
 serve: dev
-	firefox localhost:8000/ &
-	uvicorn main:app --reload --reload-include './client/dist'
+	firefox localhost:5000/ &
+	uvicorn main:app --reload --reload-include './client/dist' --port 5000
 clean:
 	mkdir -p __pycache__
 	rm -r __pycache__
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,8 @@
 import { version } from "../package.json";
 import foo from "./foo.js";
-var g = gsap;
+
 foo(document);
 console.log(version);
-var tl=gsap.timeline();
 
 function toggle(r){
     if (r == -360) return 360;
@@ -25,6 +24,7 @@ var tvars = {
     }
 }
 
+var tl=gsap.timeline();
 var tween = tl.to(".contain", tvars);
 document.querySelector(".contain").onclick = () => {
     tl.reversed( !tl.reversed() );
@@ -32,10 +32,17 @@ document.querySelector(".contain").onclick = () => {
 
 const canvas = document.getElementById('bg-canvas');
 const ctx = canvas.getContext('2d');
-let w = parseInt(window.getComputedStyle(canvas, null).getPropertyValue("width"));
-canvas.setAttribute('width', w);
-let h = parseInt(window.getComputedStyle(canvas, null).getPropertyValue("height"));
-canvas.setAttribute('height', h);
-ctx.fillStyle = "black";
-ctx.fillRect(50,50, w-100, h-100);
-console.log(w,h)
+function draw_canvas(canvas){
+    let w = parseInt(window.getComputedStyle(canvas, null).getPropertyValue("width"));
+    canvas.setAttribute('width', w);
+    let h = parseInt(window.getComputedStyle(canvas, null).getPropertyValue("height"));
+    canvas.setAttribute('height', h);
+    ctx.fillStyle = "black";
+    ctx.fillRect(50,50, w-100, h-100);
+
+    ctx.strokeStyle = "magenta";
+    ctx.lineWidth = 10;
+    ctx.strokeRect(45, 45, w-90, h-90);
+}
+draw_canvas(canvas);
+addEventListener('resize', (event) => draw_canvas(canvas));
